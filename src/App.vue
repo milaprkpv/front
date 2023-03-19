353420c54b1a2465a8ae6d80ebad46adca5886ec
milaprkpv <milanaprokopovic@gmail.com>
2023-03-19 23:23:28 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Counter from './components/Counter.vue';
+</script>
+
+<template>
+  <h1>Hello World!</h1>
+  <Counter></Counter>
+</template>
+
+<style scoped>
+  h1{
+    color: purple;
+    text-align: center;
+  }
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,32 @@
+<script setup>
+    import { reactive, ref, watchEffect, watch } from 'vue';
+    const counter = ref(0);
+    const date1 = ref(new Date().toLocaleTimeString());
+    const date2 = ref(new Date().toLocaleTimeString());
+    function counterHandler(event) {
+        console.log();
+    }
+    watchEffect(() => {
+        setInterval(() => {
+            date1.value = new Date().toLocaleTimeString()
+        }, 1000)
+    });
+    watch({}, () => {
+        setInterval(() => {
+            date2.value = new Date().toLocaleTimeString()
+        }, 1000)
+    },
+    { immediate: true });
+</script>
+
+
+<template>
+    <button @click="counterHandler">counter {{ counter }}</button>
+    <h1>{{ date1 }}</h1>
+    <h2>{{ date2 }}</h2>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
